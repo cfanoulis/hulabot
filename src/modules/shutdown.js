d12147a564d0a39e271c8377e04bad64ad70d376
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2018-09-21 22:44:03 +0300

    feat(modules/shutdown.js): Added a shutdown method UNUSED
    Added a shutdown method. It is currently unused

---
+++ src/modules/shutdown.js
@@ -0,0 +1,17 @@
+const { promisfy } = require("util");
+const unlinkFiles = promisfy(require("fs").unlinkFile);
+
+exports.run = async function() {
+    console.log("Initializing shutdown process...");
+    unlinkFiles("../../configs/tokens.json").then(
+        console.log("Tokens Cleared")
+    ).catch((err) => {
+        console.error("Error while clearing tokens. Please delete the file configs/tokens.json\nTrace: " + err);
+        process.exit(1);
+    });
+    console.log("I guess this is where we say goodbye...");
+    setTimeout(() => {
+        console.log("Goodbye :)");
+        process.exit(0);
+    }, 2000);
+};
