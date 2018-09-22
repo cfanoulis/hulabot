c0e951e86f310d4ff40842fe88cedc72c0113ba4
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2018-09-22 13:15:22 +0300

    feat(modules/functions.js): Added functions for getting current time & date

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -139,6 +139,30 @@ module.exports = (client) => {
   // `await client.wait(1000);` to "pause" for 1 second.
   client.wait = require("util").promisify(setTimeout);
 
+  //Get the current date (Courtesy of SO)
+  client.getCurrentDate = function() {
+    var objToday = new Date(),
+	weekday = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th"; }(),
+	dayOfMonth = today + ( objToday.getDate() < 10) ? "0" + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear();
+  const today = dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+  return today;
+  };
+
+  //Get the current time (Again, courtesy of SO)
+  client.getCurrentTime = function() {
+  var objToday = new Date(),
+	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+  const now = curHour + ":" + curMinute + "." + curSeconds + curMeridiem;
+  return now;
+  };
   // These 2 process methods will catch exceptions and give *more details* about the error and stack trace.
   process.on("uncaughtException", (err) => {
     const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
