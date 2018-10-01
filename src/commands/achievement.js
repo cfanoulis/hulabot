ae4bd5f7d8bdcc4cd791cd884e0c4e075c91ace1
Natsumi H <39241405+NatsumiHB@users.noreply.github.com>
2018-10-01 15:03:48 +0200

    changed displayAvatarURL

--- src/commands/achievement.js
+++ src/commands/achievement.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args, level) => {
     await message.channel.send(new MessageAttachment(
-        await client.idiotAPI.achievement(message.author.displayAvatarURL({format: "png", size: 32}), args.join(" "))
+        await client.idiotAPI.achievement(message.author.displayAvatarURL(), args.join(" "))
     ));
 }
